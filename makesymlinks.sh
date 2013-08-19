30e78db6c351e50866d262e945bf5663c9089402
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2013-08-19 13:11:18 +0200

    Add gitconfig.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -10,7 +10,7 @@
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles/backup          # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="emacs.d bashrc bash_profile offlineimaprc xmonad"
+files="bashrc bash_profile emacs.d gitconfig offlineimaprc xmonad"
 
 ##########
 

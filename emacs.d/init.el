fc854f4181029ad19f8efea057936799d80d283f
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2015-02-03 15:31:22 +0000

    Pass -shell-escape to latex for minted to work.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -141,6 +141,7 @@ point reaches the beginning or end of the buffer, stop there."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-command-extra-options "-shell-escape")
  '(clojure-defun-indents (quote (match)))
  '(colon-double-space nil)
  '(haskell-process-auto-import-loaded-modules t)

c3ea1b5e574c54e622fbecb6646e8080b56792c2
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2016-12-07 17:09:12 +0000

    Make comments have less contrast than regular text, not scream FIREBRICK at me

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -121,4 +121,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "simp" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Hack" :foundry "simp" :slant normal :weight normal :height 113 :width normal))))
+ '(font-lock-comment-face ((t (:foreground "dark gray")))))

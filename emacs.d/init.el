de04f8ef8b889ddf720b6157926d3231c16f8f1d
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2017-08-17 16:33:37 +0100

    yaml-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -127,6 +127,10 @@
   :defer t ;; not sure this is harmful, needed, or useful..
   :init (add-hook 'coq-mode-hook #'company-coq-mode))
 
+(use-package yaml-mode
+  :ensure t
+  :mode "\\.yaml\\'")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -149,8 +153,8 @@
     (".agdai .run.xml" ".bcf" ".hi" ".cb" ".cb2" ".cmti" ".cmt" ".annot" ".cmi" ".cmxa" ".cma" ".cmx" ".cmo" ".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".log" ".out" ".synctex.gz" ".pdf" ".fdb_latexmk" ".dvi" ".fls" ".spl" ".glob" ".v.d" ".vo" ".ibc")))
  '(ido-enable-flex-matching t)
  '(inhibit-startup-screen t)
- '(proof-splash-enable nil)
  '(org-agenda-files (quote ("~/Documents/journal.org")))
+ '(proof-splash-enable nil)
  '(safe-local-variable-values (quote ((TeX-master)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
